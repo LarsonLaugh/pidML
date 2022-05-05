dad5420470ea42a6c64ed3941032de6cdbf9e9f6
lixian <lixian.philips.wang@gmail.com>
2022-05-05 10:17:08 +0200

    debug

--- pidtrain.py
+++ pidtrain.py
@@ -58,20 +58,19 @@ class PID():
         return self.curr_cycle
 
     def output(self, temp):
-        error = self.get_setpoint() - temp
-        integral = self.get_integral() + error
-        derivative = (error - self.get_last_error()) / self.get_period() / 1e3
-        kp, ki, kd = self.get_pid()
+        error = self.setpoint - temp
+        integral = self.integral + error
+        derivative = (error - self.last_error) / self.period / 1e3
         self.set_integral(integral)
         self.set_last_error(error)
-        return kp * error + ki * integral + kd * derivative
+        return self.kp * error + self.ki * integral + self.kd * derivative
 
     def simulation(self, cycle_num):
         output_logger = []
         temp_logger = []
         self.set_curr_cycle(1)
-        while cycle_num + 1 > self.get_curr_cycle():
-            temp_curr = self.get_temp()
+        while cycle_num + 1 > self.curr_cycle:
+            temp_curr = self.temp
             temp_logger.append(temp_curr)
             output = self.output(temp_curr)
             output_logger.append(output)
@@ -80,8 +79,8 @@ class PID():
             else:
                 self.set_temp(temp_curr - 0.01)
             # print(self.get_curr_cycle(), self.get_temp())
-            sleep(self.get_period())
-            self.set_curr_cycle(self.get_curr_cycle() + 1)
+            sleep(self.period)
+            self.set_curr_cycle(self.curr_cycle + 1)
         return temp_logger, output_logger
 
     @staticmethod
@@ -123,14 +122,12 @@ def genalg_simu(GenMax, PopSize, setpoint, period, cycle_num, is_mae=True):
         # First generation
         if gen == 0:
             K = 100 * random_sample((PopSize, 3))
-        Cost = []
         pid_data_gen = pd.DataFrame()
         # run simulation for each
         for i in range(PopSize):
             pid = PID(K[i][0], K[i][1], K[i][2], setpoint, period)
             cost = pid.mae_cost(pid.simulation(cycle_num)[0], setpoint) if is_mae else pid.mse_cost(
                 pid.simulation(cycle_num)[0], setpoint)
-            Cost.append(cost)
             print(K[i][0], K[i][1], K[i][2], cost)
             pid_data_gen = pid_data_gen.append(
                 {'Kp': K[i][0], 'Ki': K[i][1], 'Kd': K[i][2], 'Cost': cost, 'Gen': int(gen)},
@@ -145,8 +142,10 @@ def genalg_simu(GenMax, PopSize, setpoint, period, cycle_num, is_mae=True):
         # Elitism
         K_prime = K
         # hybrid
-        for j in np.linspace(1, PopSize - 3, 1, dtype='int'):
+        j = 1
+        while j <= PopSize - 3:
             K_prime[j], K_prime[j + 1] = pid_hybrid(K[j], K[j + 1])
+            j += 2
         if PopSize % 2:
             K_prime[PopSize - 1] = pid_mutation()  # the last to mutate
         else:
@@ -161,7 +160,7 @@ if __name__ == "__main__":
     setpoint = 65
     period = 0.001
     GenMax = 100
-    PopSize = 19
+    PopSize = 20
     cycle_num = 1000
 
     pid_data = genalg_simu(GenMax, PopSize, setpoint, period, cycle_num, True)
